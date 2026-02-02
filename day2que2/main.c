9d2ccced96023d4569a8fd0af3c29d6c7089e11d
sanaa <Sanaakumar07@gmail.com>
2026-02-02 19:08:57 +0530

    Initial Commit

---
+++ day2que2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  day2que2
+//
+//  Created by Sanaa Kumar on 02/02/26.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return EXIT_SUCCESS;
+}
