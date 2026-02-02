6fc978b893f90d35544cacf5ec35a56c7ff9d6f1
sanaa <Sanaakumar07@gmail.com>
2026-02-02 19:43:57 +0530

    c

--- day2que2/main.c
+++ day2que2/main.c
@@ -1,15 +1,22 @@
-//
-//  main.c
-//  day2que2
-//
-//  Created by Sanaa Kumar on 02/02/26.
-//
-
-#include <stdlib.h>
 #include <stdio.h>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return EXIT_SUCCESS;
+int main(void)
+{
+int n, pos;
+int arr[100];
+scanf("%d", &n);
+for (int i = 0; i < n; i++)
+{
+scanf("%d", &arr[i]);
 }
+scanf("%d", &pos);
+for (int i = pos - 1; i < n - 1; i++)
+{
+arr[i] = arr[i + 1];
+}
+for (int i = 0; i < n - 1; i++)
+{
+printf("%d ", arr[i]);
+}
+return 0;
+}
+
